984a3bca2bafe0cc41ed7140c871b15adccf2f91
dndphu <dndphu@gmail.com>
2021-10-07 14:27:29 +0700

    init pj

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,25 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about" style="position: relative">
+    <marquee direction="down" width="700" height="600" behavior="alternate">
+      <marquee behavior="alternate" scrollamount="3">
+        This text will bounce
+      </marquee>
+      <marquee behavior="alternate" scrollamount="2" loop="1"> aaaaa </marquee>
+      <marquee behavior="alternate" scrollamount="7"> bbbbbbbbbb </marquee>
+      <marquee behavior="alternate" scrollamount="10"> cccccccccccc </marquee>
+      <marquee behavior="alternate" scrollamount="9">
+        Phú k vâu nhưng keep it real</marquee
+      >
+    </marquee>
+
+    <marquee behavior="scroll" direction="left" scrollamount="3"
+      >Slow speed scroll speed</marquee
+    >
+    <marquee behavior="scroll" direction="left" scrollamount="10"
+      >Medium speed scroll speed</marquee
+    >
+    <marquee behavior="scroll" direction="left" scrollamount="17"
+      >Fast speed scroll speed</marquee
+    >
   </div>
 </template>
